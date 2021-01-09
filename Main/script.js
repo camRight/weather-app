6cb4e8c68e23731d1a851f608fb91f6cdc3c7a24
Cameron Wright <cam.mac.wright@gmail.com>
2021-01-09 11:50:29 -0800

    first fetch & attached button

--- Main/script.js
+++ Main/script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   // code...
 
-function search_city() {
+  function search_city() {
     let input = document.getElementById('city-search').value
-    input=input.toLowerCase();
+    input = input.toLowerCase();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=e6c064d18459b94b71b91664234d49f9`)
+      .then(response => response.json())
+      .then(data => console.log(data));
 
-fetch('api.openweathermap.org/data/2.5/weather?q={`input`}&appid={e6c064d18459b94b71b91664234d49f9}')
-  .then(response => response.json())
-  .then(data => console.log(data));
-}
+  }
+  document.getElementById("city-click-btn").addEventListener("click", search_city); 
 });
