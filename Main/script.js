9baac4608dcec6a4d4f2a4458b4fdbbd25c724c4
Cameron Wright <cam.mac.wright@gmail.com>
2021-01-11 21:12:36 -0800

    float box model for alignment rather than nesting

--- Main/script.js
+++ Main/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   // code...
 
-  function search_city() {
-    let input = document.getElementById('city-search').value
+  function searchCity() {
+    let input = document.getElementById('city-search').value;
     input = input.toLowerCase();
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=e6c064d18459b94b71b91664234d49f9`)
       .then(response => response.json())
       .then(data => console.log(data));
 
   }
-  document.getElementById("city-click-btn").addEventListener("click", search_city); 
+  document.getElementById("city-click-btn").addEventListener("click", searchCcity); 
 });
