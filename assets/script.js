d1c7d3d56ea04630c98227bc5fd42ba7de783205
Cameron Wright <cam.mac.wright@gmail.com>
2021-01-14 20:00:54 -0800

    type error: http rather than https created 404 on fetch

--- assets/script.js
+++ assets/script.js
@@ -20,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function () {
       .then(response => response.json())
       .then(data => {
         console.log(data)
-        fetch(`http://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=e6c064d18459b94b71b91664234d49f9`)
+        fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${data.coord.lat}&lon=${data.coord.lon}&appid=e6c064d18459b94b71b91664234d49f9`)
           .then(response => response.json())
           .then(uvdata => {
 
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
             let uvIndex = uvdata.value;
             let currentDate = moment(data.dt, "X").format("(MM/DD/YYYY)");
             let iconcode = data.weather[0].icon;
-            let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            let iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
             document.getElementById("dashboard").innerHTML = ` 
       
             <h3 id="city-title"> ${data.name} ${currentDate} <img src="${iconurl}"/>  </h3>
@@ -58,7 +58,7 @@ document.addEventListener("DOMContentLoaded", function () {
           if (fiveDay.list[i].dt_txt.includes("15:00:00")) {
 
             let iconcode = fiveDay.list[i].weather[0].icon;
-            let iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            let iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
             let fiveTemp = fiveDay.list[i].main.temp;
             let nextDay = moment(fiveDay.list[i].dt, "X").format("dddd");
             let fiveHum = fiveDay.list[i].main.humidity;
